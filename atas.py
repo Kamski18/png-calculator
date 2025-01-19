8be58b59f4d50dd7bb1530e754722fd29c1bdb07
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2025-01-20 01:22:27 +0800

    improvements

--- atas.py
+++ atas.py
@@ -1,53 +1,72 @@
-credithour = {
-    "BM" : 6,
-    "BI" : 6,
-    "SEJ" : 4,
-    "MAT" : 6,
-    "ADDMATH" : 6,
-    "BIO" : 6,
-    "PHY" : 6,
-    "CHEM" : 6,
-    "PAI" : 5,
-    "PJK" : 3,
-    
+import sys
+# pylint: disable=unused-variable,missing-function-docstring
+
+CREDIT_HOUR: dict[str, int] = {
+    "BM": 6,
+    "BI": 6,
+    "SEJ": 4,
+    "MATH": 6,
+    "ADDMATH": 6,
+    "BIO": 6,
+    "PHYS": 6,
+    "CHEM": 6,
+    "PAI": 5,
+    "PJK": 3,
+    "SENIREKA": 2,
+    "SK": 2,
 }
 
-weight = {
-    "A" : 4.0,
-    "A-" : 3.67,
-    "B+" : 3.33,
-    "B" : 3.0,
-    "B-" : 2.67,
-    "C+" : 2.33,
-    "C" : 2.0,
-    "C-" : 1.67,
-    "D+" : 1.33,
-    "D" : 1.0,
-    "D-" : 0.67,
-    "E" : 0.0,
-    "F" : 0.0
-    
+GRADE_WEIGHTAGES: dict[str, float] = {
+    "L": 3.820925926,
+    "A+": 4.0,
+    "A": 4.0,
+    "A-": 3.67,
+    "B+": 3.33,
+    "B": 3.0,
+    "B-": 2.67,
+    "C+": 2.33,
+    "C": 2.0,
+    "C-": 1.67,
+    "D+": 1.33,
+    "D": 1.0,
+    "D-": 0.67,
+    "E": 0.0,
+    "F": 0.0,
 }
 
-def calculate():
-    marks = input("Enter your grade in order from up to down (space separated): ").split(" ")
+weightages: dict[str, float] = {
+    "BM": 0,
+    "BI": 0,
+    "SEJ": 0,
+    "MATH": 0,
+    "ADDMATH": 0,
+    "BIO": 0,
+    "PHYS": 0,
+    "CHEM": 0,
+    "PAI": 0,
+    "PJK": 0,
+    "SENIREKA": 0,
+    "SK": 0,
+}
 
-    while len(marks) != len(credithour):
-        print("Error: Number of subjects entered is not equal to the number of subjects in the list")
-        marks = input("Enter your grade in order from up to down (space separated): ").split(" ")
 
-    totalcredits = 0
-    total = 0
+def calculate_png() -> float:
+    grades: list[str] = []
+    for subject in weightages:
+        user_input: str = input(f"Enter grade for {subject}: ").upper()
+        if user_input not in GRADE_WEIGHTAGES:
+            print("Invalid grade!")
+            sys.exit(1)
 
-    for i, (subject, credit) in enumerate(credithour.items()):
-        grade = marks[i].capitalize()
-        if grade in weight:
-            total += weight[grade] * credit
-            totalcredits += credit
-        else:
-            print(f"Error: Invalid grade '{grade}' for subject '{subject}'")
-            return
+        weightages[subject] = GRADE_WEIGHTAGES[user_input.upper()]
 
-    gpa = total / totalcredits
-    print(f"Your GPA is: {gpa:.3f}")
-calculate()
+    product_of_credhour_weightages: float = 0
+    total_credit_hours: int = sum(CREDIT_HOUR.values())
+
+    for subject, credit_hour in CREDIT_HOUR.items():
+        product_of_credhour_weightages += credit_hour * weightages[subject]
+
+    return product_of_credhour_weightages / total_credit_hours
+
+
+print(f"{calculate_png():.3f}")
